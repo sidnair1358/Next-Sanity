d1f393f415419f88bafa5ded0c69a8c86e30c72a
sidnair1358 <sidnair1358@gmail.com>
2023-03-25 16:13:20 +0000

    Complete Navbar coomponent

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import {
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiFillFacebook,
+  AiFillLinkedin,
+} from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p>Luxury Perfume Collection, All rights reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiFillFacebook />
+        <AiFillLinkedin />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Ecom App</Link>
+      </p>
+      <button className="cart-icon" type="button" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
